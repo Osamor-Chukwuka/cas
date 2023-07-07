b3745956414341ffff41445b7c3816d2370515fc
osamor chukwuka <osamorchukwuka15@gmail.com>
2023-07-07 01:26:31 +0100

    renamed the migration and model files for the replies

--- database/migrations/2023_07_07_002531_create_commendreply_table.php
+++ database/migrations/2023_07_07_002531_create_commendreply_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('reply', function (Blueprint $table) {
+        Schema::create('commendreply', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('reply');
+        Schema::dropIfExists('commendreply');
     }
 };
